fa9d967f9bab9d3d42ebf63bd1b2bfa00cdc7b83
Ars <arsershoff@gmail.com>
2022-03-16 08:47:13 +0300

    adaptive mobile

---
+++ main.js
@@ -0,0 +1,39 @@
+const main = () => {
+  Array.from(document.querySelectorAll(".headNavigation .leftBlock a")).forEach(
+    (link) => {
+      link.onclick = (event) => {
+        event.preventDefault();
+        const selector = link.getAttribute("href");
+        scrollToElement(document.querySelector(selector));
+      };
+    }
+  );
+
+  const form = document.querySelector("form.registrationBlock");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const inputs = Array.from(form.querySelectorAll("input"));
+    const payload = Object.fromEntries(
+      inputs.map((input) => [input.name, input.value])
+    );
+
+    console.log(payload);
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(payload),
+    }).then((res) => {
+      console.log(res);
+    });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", main);
+
+const scrollToElement = (el, offset = 80) =>
+  window.scrollTo({
+    top: el.getBoundingClientRect().top + window.pageYOffset - offset,
+    behavior: "smooth",
+  });
